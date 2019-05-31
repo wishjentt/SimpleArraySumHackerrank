953cee0fcd7cfa6651439e80fe46f36424d6dd2c
wishjentt <wishjentt@github.com>
2019-05-31 18:15:29 +0300

    Commit Staged

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static int simpleArraySum(int[] ar)
+        {
+            int total = 0;
+            foreach (int n in ar)
+            {
+                total += n;
+            }
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = simpleArraySum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
